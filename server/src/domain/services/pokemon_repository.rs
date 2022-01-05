76c303a6a25b7e98539162e806619b83992b3870
tsuchinoko0402 <okazaki@zakioka.net>
2022-01-05 15:29:46 +0900

    [add]ドメインサービス追加

---
+++ server/src/domain/services/pokemon_repository.rs
@@ -0,0 +1,26 @@
+//! ポケモンに関するドメインサービスを定義する。
+
+use crate::domain::models::pokemon::{pokemon::Pokemon, pokemon_number::PokemonNumber};
+use anyhow::Result;
+
+/// Pokemon のリポジトリインタフェース
+pub trait PokemonRepository {
+    /// 番号からポケモンを探す
+    fn find_by_number(&self, number: &PokemonNumber) -> Option<Pokemon>;
+    ///
+    fn list(&self) -> Result<Vec<Pokemon>>;
+    /// オブジェクトを永続化（保存）する振る舞い
+    fn insert(&self, pokemon: &Pokemon) -> Result<()>;
+    /// オブジェクトを再構築する振る舞い
+    fn update(&self, pokemon: &Pokemon) -> Result<()>;
+    /// オブジェクトを永続化（破棄）する振る舞い
+    fn delete(&self, pokemon: &Pokemon) -> Result<()>;
+    /// 目的：作成したポケモンの重複確認を行う。
+    /// exists: (Pokemon) -> bool
+    fn exists(&self, pokemon: &Pokemon) -> bool {
+        match self.find_by_number(&pokemon.number) {
+            Some(_) => true,
+            None => false,
+        }
+    }
+}
