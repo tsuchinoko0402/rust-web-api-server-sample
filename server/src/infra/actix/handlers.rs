758cd104223fbf59844f67bd30a7e87f4285a771
tsuchinoko0402 <okazaki@zakioka.net>
2022-01-08 18:51:49 +0900

    [update]レスポンスの内容を変更

--- server/src/infra/actix/handlers.rs
+++ server/src/infra/actix/handlers.rs
@@ -6,6 +6,13 @@ use crate::application::pokemon_update_service::{PokemonUpdateService, PokemonUp
 use crate::application::{pokemon_data::PokemonData, pokemon_register_service::PokemonRegisterService};
 use crate::infra::actix::request::PokemonRequest;
 use actix_web::{delete, get, post, put, web, web::Json, HttpResponse, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    message: String,
+    r#type: String,
+}
 
 #[post("/pokemon")]
 async fn post_pokemon(
@@ -13,9 +20,18 @@ async fn post_pokemon(
     request: Json<PokemonRequest>,
 ) -> impl Responder {
     let pokemon_application = PokemonRegisterService::new(data.pokemon_repository());
-    match pokemon_application.handle(PokemonData::new(request.of())) {
-        Ok(_) => HttpResponse::NoContent().finish(),
-        Err(_) => HttpResponse::InternalServerError().json(""),
+    let data = PokemonData::new(request.of());
+    match pokemon_application.handle(data.clone()) {
+        Ok(_) => {
+            HttpResponse::Ok().body("SUCCESS Register Pokemon")
+        },
+        Err(_) => {
+            let response = ErrorResponse {
+                message: format!("FAILURE Register Pokemon: {:?}", data.clone()),
+                r#type: "get_pokemon_error".to_string(),
+            };
+            HttpResponse::InternalServerError().json(response)
+        },
     }
 }
 
@@ -25,9 +41,18 @@ async fn get_pokemon(
     path_params: web::Path<(i32,)>,
 ) -> impl Responder {
     let pokemon_application = PokemonGetService::new(data.pokemon_repository());
-    match pokemon_application.handle(path_params.into_inner().0.into()) {
-        Ok(pokemon) => HttpResponse::Ok().json(pokemon),
-        Err(_) => HttpResponse::InternalServerError().json(""),
+    let no = path_params.into_inner().0.into();
+    match pokemon_application.handle(no) {
+        Ok(pokemon) => {
+            HttpResponse::Ok().json(pokemon)
+        },
+        Err(_) => {
+            let response = ErrorResponse {
+                message: format!("FAILURE Get Pokemon: no {:?}", no),
+                r#type: "get_pokemon_list_error".to_string(),
+            };
+            HttpResponse::InternalServerError().json(response)
+        },
     }
 }
 
@@ -36,7 +61,13 @@ async fn get_pokemon_list(data: web::Data<RequestContext>) -> impl Responder {
     let pokemon_application = PokemonListService::new(data.pokemon_repository());
     match pokemon_application.handle() {
         Ok(pokemon) => HttpResponse::Ok().json(pokemon),
-        Err(_) => HttpResponse::InternalServerError().json(""),
+        Err(_) => {
+            let response = ErrorResponse {
+                message: "FAILURE Get Pokemon List".to_string(),
+                r#type: "get_pokemon_list_error".to_string(),
+            };
+            HttpResponse::InternalServerError().json(response)
+        },
     }
 }
 
@@ -47,12 +78,19 @@ async fn update_pokemon(
     request: Json<PokemonRequest>,
 ) -> impl Responder {
     let pokemon_application = PokemonUpdateService::new(data.pokemon_repository());
-    let mut update_command = PokemonUpdateCommand::new(path_params.into_inner().0.into());
+    let no = path_params.into_inner().0.into();
+    let mut update_command = PokemonUpdateCommand::new(no);
     update_command.set_name(Some(request.of().name.into()));
     update_command.set_types(Some(request.of().types.into()));
     match pokemon_application.handle(update_command) {
-        Ok(_) => HttpResponse::NoContent().finish(),
-        Err(_) => HttpResponse::InternalServerError().json(""),
+        Ok(_) => HttpResponse::Ok().body(format!("SUCCESS Update Pokemon: no {}", no)),
+        Err(_) => {
+            let response = ErrorResponse {
+                message: format!("FAILURE Update Pokemon: no {}", no),
+                r#type: "update_pokemon_error".to_string(),
+            };
+            HttpResponse::InternalServerError().json(response)
+        },
     }
 }
 
@@ -62,9 +100,16 @@ async fn delete_pokemon(
     path_params: web::Path<(i32,)>,
 ) -> impl Responder {
     let pokemon_application = PokemonDeleteService::new(data.pokemon_repository());
-    match pokemon_application.handle(path_params.into_inner().0.into()) {
-        Ok(_) => HttpResponse::NoContent().finish(),
-        Err(_) => HttpResponse::InternalServerError().json(""),
+    let no = path_params.into_inner().0.into();
+    match pokemon_application.handle(no) {
+        Ok(_) => HttpResponse::Ok().body(format!("SUCCESS Delete Pokemon: no {}", no)),
+        Err(_) => {
+            let response = ErrorResponse {
+                message: format!("FAILURE Delete Pokemon: no {}", no),
+                r#type: "delete_pokemon_error".to_string(),
+            };
+            HttpResponse::InternalServerError().json(response)
+        },
     }
 }
 
